f7ce6c07ffa2e3c1f2d5e9022808a42399a32238
zenon-tan <zenontanxh@gmail.com>
2022-12-08 15:49:15 +0800

    Added revision

--- revision/src/SocketApp.java
+++ revision/src/SocketApp.java
@@ -1,3 +1,5 @@
+package revision.src;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
@@ -19,12 +21,13 @@ public class SocketApp {
     // java SocketApp client localhost 12345
 
     public static void main(String[] args) {
+        // command line arguments
+        // The Program will only run with the terminal
 
-        // command line arguments are space separated (argnet)
-        // Whatever is after the filename are argnets
-        // These don't run using the play button, it requires input with the terminal
-
-        if (args.length < 3) {
+        // We need 3 arguments after running the program:
+        // <server or client> <host/port> <filename/port>
+        
+        if(args.length < 3) {
             System.out.println("""
 
                     Incorrect inputs, please check the following usage:
@@ -38,162 +41,132 @@ public class SocketApp {
                     > Usage: Client
                     <program> <'client'> <host> <port>
                     ================
-
+                    
                     """);
-            return;
+                return;
         }
 
         String type = args[0];
-        if (type.equalsIgnoreCase("server")) {
-            int port = Integer.parseInt(args[1]);
-            String fileName = args[2];
-            startServer(port, fileName);
 
-
-        } else if (type.equalsIgnoreCase("client")) {
-            String hostName = args[1];
-            int port = Integer.parseInt(args[2]);
-            startClient(hostName, port);
-
-        } else if(type.equalsIgnoreCase("thread.server")) {
-            int port = Integer.parseInt(args[1]);
-            String fileName = args[2];
-            startMultiThreadServer(port, fileName);
+        switch(type.toLowerCase().trim()) {
+                case "server":
+                int port = Integer.parseInt(args[1]);
+                String fileName = args[2];
+                startServer(port, fileName);
+                break;
+            case "client":
+                String hostName = args[1];
+                int port2 = Integer.parseInt(args[2]);
+                startClient(hostName, port2);
+                break;
+            case "thread.server":
+                break;
+            default:
+                System.out.println("Incorrect argnets");
+                break;
         }
-         else {
-            System.out.println("Incorrect argnets");
-        }
-
-        // Usage: Server
-        // <program> <server> <port> <filename>
 
     }
 
-    public static void startServer(int port, String fileName) {
+    public static void startServer(int port, String filename) {
 
         ServerSocket server;
 
+        // Initalises a server and create input and output streams
         try {
+
             server = new ServerSocket(port);
             Socket socket = server.accept();
 
-            // INPUT STRING
+            //INPUT STREAM
             DataInputStream dis = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
 
-            // OUTPUT STRING
+            //OUTPUT STREAM
             DataOutputStream dos = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
 
-            while (true) {
+            while(true) {
+                // Read the first line
                 String fromClient = dis.readUTF();
-                if (fromClient.equalsIgnoreCase("get-cookie")) {
-                    Cookie cookie = new Cookie(fileName);
-                    String randomCookie = cookie.returnRandomCookie();
-                    
-                    // Send "cookie-text" to client
+                if(fromClient.equalsIgnoreCase("get-cookie")) {
+                    // Generate cookie instance and return a random cookie
+                    Cookie cookie = new Cookie(filename);
+                    // Send cookie-text to client
                     dos.writeUTF("cookie-text");
+                    // Send the random cookie string to client
+                    String randomCookie = cookie.getRandomCookie();
                     dos.writeUTF(randomCookie);
-                    dos.flush();
-                } else if (fromClient.equalsIgnoreCase("exit")) {
+
+                } else if(fromClient.equalsIgnoreCase("exit")) {
                     break;
                 } else {
                     System.out.println("Invalid command");
                     dos.writeUTF("From server: Invalid command");
                     dos.flush();
                 }
+                
             }
-
+            dos.close();
+            dis.close();
+                
+                
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-    }
-
-    public static void startMultiThreadServer(int port, String fileName) {
-
-        ServerSocket server;
-
-        try {
-
-            server = new ServerSocket(port);
-            while(true) {
-
-                Socket socket = server.accept();
-                // INPUT STRING
-                DataInputStream dis = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
-    
-                // OUTPUT STRING
-                DataOutputStream dos = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
-
-                Thread tsh = new ThreadSocketHandler(socket, dis, dos, fileName);
-                tsh.start();
-
-            }
-
-
-
-        } catch (IOException e) {
-            e.getStackTrace();
-        }
 
     }
-
-    // Usage: Client
-    // <program> <client> <host> <port>
 
     public static void startClient(String host, int port) {
 
         try {
+
             Socket socket = new Socket(host, port);
 
-            // INPUT STRING
+            //INPUT STREAM
             DataInputStream dis = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
 
-            // OUTPUT STRING
+            //OUTPUT STREAM
             DataOutputStream dos = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
 
-            // Scanner
+            // Get input from terminal
             Scanner sc = new Scanner(System.in);
-            boolean stop = false;
 
-            while (!stop) {
+            while(true) {
                 String line = sc.nextLine();
-                if (line.equalsIgnoreCase("exit")) {
+                if(line.equalsIgnoreCase("exit")) {
                     dos.writeUTF("exit");
                     dos.flush();
-                    stop = true;
                     break;
-                }
 
-                if (line.equalsIgnoreCase("get-cookie")) {
-                    // Send a request to the server for a cookie
+                } else if(line.equalsIgnoreCase("get-cookie")) {
+                    // Send "get-cookie" to server
                     dos.writeUTF("get-cookie");
                     dos.flush();
 
-                    // Receiving from server
+                    // Recieve "cookie-text" and random cookie from server
                     String fromServer = dis.readUTF();
-                    //System.out.println("Response from server: " + fromServer);
 
                     if(fromServer.equalsIgnoreCase("cookie-text")) {
                         String randomCookie = dis.readUTF();
                         System.out.println(randomCookie);
-    
-    
                     }
-                }
 
-                else {
+
+
+                } else {
                     System.out.println("Invalid command: " + line);
-
+                    dis.close();
+                    dos.close();
                 }
-
+                
             }
 
+            
         } catch (UnknownHostException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
     }
-
+    
 }
